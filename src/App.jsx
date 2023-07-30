5bb1073d3f04621e8e202572a0a04160d9eb80c1
Olga <parlelya@vk.com>
2023-07-30 23:03:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import './App.css';
+import React from "react";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Аутентификация с Firebase API</h1>
+      <h1>Аутентификация с Firebase API</h1>
       <Login />
       <Registration />
     </div>
